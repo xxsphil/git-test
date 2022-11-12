1e540a0c4618098bbedb90643fde3ac757da2f6d
xxsphil <117904961+xxsphil@users.noreply.github.com>
2022-11-12 11:44:49 +0800

    Update gittest.java

--- src/main/java/test/gittest.java
+++ src/main/java/test/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello push test git");
+        System.out.println("program to idea");
     }
 }
